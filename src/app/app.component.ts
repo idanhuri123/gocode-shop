e8b37dda145aace14c5d56599fc305ef2dac3044
Idan <Idanhuri@gmail.com>
2021-01-19 19:18:38 +0200

    F

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'gocode-shop';
+  title: string = 'gocode-shop';
+  show: boolean = true;
+
+  showNHide() {
+    this.title = this.title?'':'gocode-shop';
+  }
 }
-   
