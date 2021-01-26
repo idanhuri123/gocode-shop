7449a8d026dd5568f00ae55ce5d57ed874718d5b
Idan <Idanhuri@gmail.com>
2021-01-26 20:24:22 +0200

    update docs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title: string = 'gocode-shop2';
+  title: string = 'gocode-shop3';
   show: boolean = true;
 
   showNHide() {
